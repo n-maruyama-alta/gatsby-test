d2aadfb87ed1b464a2896a556ad61ab48dd0bf0d
n_maruyama <n_maruyama@alta.co.jp>
2022-08-29 15:44:51 +0900

    更新テスト

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,7 +9,7 @@ export default () => (
         <Link to="/">Home</Link>
       </li>
       <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
+        <Link to="/blog/">ブログ</Link>
       </li>
     </ul>
   </nav>
